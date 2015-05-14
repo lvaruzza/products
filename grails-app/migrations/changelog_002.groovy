cc4f236cb4963f94f6eef32c1f12b8f71598b7a5
Leonardo Varuzza <varuzza@gmail.com>
2015-05-14 11:50:58 -0300

    Add migrations 002

---
+++ grails-app/migrations/changelog_002.groovy
@@ -0,0 +1,26 @@
+databaseChangeLog = {
+
+	changeSet(author: "varuzza (generated)", id: "1431559468404-1") {
+		modifyDataType(columnName: "updated_on", newDataType: "date", tableName: "price")
+	}
+
+	changeSet(author: "varuzza (generated)", id: "1431559468404-2") {
+		createIndex(indexName: "price_sku", tableName: "price") {
+			column(name: "sku")
+		}
+	}
+
+	changeSet(author: "varuzza (generated)", id: "1431559468404-3") {
+		createIndex(indexName: "price_updated_on", tableName: "price") {
+			column(name: "updated_on")
+		}
+	}
+
+	changeSet(author: "varuzza", id: "1431559468404-4") {
+	       sql("""
+create view last_price as
+select distinct on (sku) sku,price_USD,price_BRL,price_BRL_no_IPI,updated_on
+from price
+order by sku,updated_on  desc;""")
+	}
+}
